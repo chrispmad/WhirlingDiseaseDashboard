4147b78e5966b43b179f3c325f82e143e6a5ba98
Madsen <Chris.Madsen@gov.bc.ca>
2025-05-06 14:27:12 -0700

    added section to test if LAN data file looks extremely similar to local and if so copy it locally

--- make_objs_for_dashboard.R
+++ make_objs_for_dashboard.R
@@ -5,14 +5,28 @@ library(bcmaps)
 library(readxl)
 library(bcdata)
 
-# Data
-# fish = read_excel('data/2024 WD sampling results_fish_dat_MASTER LIST.xlsx', sheet = "Fish sampling")
-dat = read_excel('data/2024 WD sampling results_fish_eDNA_MASTER LIST.xlsx', sheet = "eDNA")
+lan_folder = "//SFP.IDIR.BCGOV/S140/S40203/RSD_ FISH & AQUATIC HABITAT BRANCH/General/"
+proj_wd = getwd()
+onedrive_wd = paste0(str_extract(getwd(),"C:/Users/[A-Z]+/"),"OneDrive - Government of BC/data/")
 
-# fish = purrr::set_names(fish, snakecase::to_snake_case)
+# Data
+# Copy excel results from LAN folder IF it has all the right columns. This is
+# intended to save us from overwriting the local data with some garbage 
+# LAN file that has replaced our goldenboy data file.
+new_potential_dat_file = read_excel(paste0(lan_folder,"2 SCIENCE - Invasives/SPECIES/Whirling Disease/Monitoring/WD_sampling_results_fish_eDNA_used_for_making_maps_CMADSEN.xlsx"), sheet = "eDNA")
+local_data_file = read_excel('data/WD sampling results_fish_eDNA_MASTER LIST.xlsx', sheet = "eDNA")
+
+if(identical(names(new_potential_dat_file),names(local_data_file)) & nrow(new_potential_dat_file) == nrow(local_data_file)){
+  print("New data file has identical column names and number of rows. Copying locally...")
+  file.copy(from = paste0(lan_folder,"2 SCIENCE - Invasives/SPECIES/Whirling Disease/Monitoring/WD_sampling_results_fish_eDNA_used_for_making_maps_CMADSEN.xlsx"),
+            to = paste0('data/WD sampling results_fish_eDNA_MASTER LIST.xlsx'),
+            overwrite = T)
+}
+
+dat = read_excel('data/WD sampling results_fish_eDNA_MASTER LIST.xlsx', sheet = "eDNA")
+
 dat = purrr::set_names(dat, snakecase::to_snake_case)
 
-# fish = fish |> filter(!is.na(lat) & !is.na(long))
 dat = dat |> filter(!is.na(lat) & !is.na(long))
 
 # Split rows that have both eDNA and fish sampling into two rows each.
