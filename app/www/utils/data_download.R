689a5f9e0e05d8da50c8743af42242c6fa29da46
Phelan <John.Phelan@gov.bc.ca>
2026-01-14 15:31:34 -0800

    download each year seperately

--- app/www/utils/data_download.R
+++ app/www/utils/data_download.R
@@ -1,82 +1,96 @@
-output$data_dl <- downloadHandler(
-  filename = function() {
-    paste0('BC_WhirlingDisease_', 
-           stringr::str_extract(lubridate::with_tz(Sys.Date(), tzone = "America/Vancouver"), "[0-9]{4}-[0-9]{2}-[0-9]{2}"),
-           '.xlsx')
-  },
-  content = function(con) {
-    my_wb = openxlsx::createWorkbook()
-    openxlsx::addWorksheet(my_wb, "Results")
-    openxlsx::addWorksheet(my_wb, "Species Look-up")
-    
-    results_for_download = dat |>
-      dplyr::group_by(sample_site_name) |>
-      dplyr::mutate(sampling_method = paste0(sampling_method, collapse = ' + ')) |>
-      dplyr::ungroup() |>
-      dplyr::distinct() |>
-      dplyr::mutate(Latitude = round(sf::st_coordinates(geom)[,2],4),
-                    Longitude = round(sf::st_coordinates(geom)[,1],4)) |>
-      dplyr::mutate(fish_sampling_results_q_pcr_mc_detected = tidyr::replace_na(fish_sampling_results_q_pcr_mc_detected, "NA")) |>
-      dplyr::mutate(fish_species_sampled = tidyr::replace_na(fish_species_sampled, "NA")) |>
-      dplyr::mutate(e_dna_results_mc = tidyr::replace_na(e_dna_results_mc, "NA")) |>
-      dplyr::mutate(e_dna_results_tubifex = tidyr::replace_na(e_dna_results_tubifex, "NA")) |>
-      sf::st_drop_geometry() |>
-      dplyr::select(`Sample Site` = sample_site_name,
-                    Latitude,
-                    Longitude,
-                    `Sampling Method` = sampling_method,
-                    `Fish Species Sampled` = fish_species_sampled,
-                    `Fish Sampling Results` = fish_sampling_results_q_pcr_mc_detected,
-                    `eDNA Sampling Results (M. cerebralis - parasite)` = e_dna_results_mc,
-                    `eDNA Sampling Results (Tubifex worm)` = e_dna_results_tubifex) |>
-      dplyr::distinct() |>
-      dplyr::arrange(`Sample Site`)
+# Helper function to create Excel workbook
+create_wb_for_year <- function(year, con, dat_dl) {
+  dat_year <- dat_dl %>% dplyr::filter(year == year)
+  
+  wb <- openxlsx::createWorkbook()
+  openxlsx::addWorksheet(wb, "Results")
+  openxlsx::addWorksheet(wb, "Species Look-up")
+  
+  if(nrow(dat_year) > 0) {
+    results <- dat_year %>%
+      dplyr::group_by(sample_site_name) %>%
+      dplyr::mutate(sampling_method = paste0(sampling_method, collapse = " + ")) %>%
+      dplyr::ungroup() %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(
+        Latitude  = if("geom" %in% names(.) & !all(is.na(geom))) round(sf::st_coordinates(geom)[, 2], 4) else NA,
+        Longitude = if("geom" %in% names(.) & !all(is.na(geom))) round(sf::st_coordinates(geom)[, 1], 4) else NA
+      ) %>%
+      dplyr::mutate(
+        dplyr::across(
+          c(
+            fish_sampling_results_q_pcr_mc_detected,
+            fish_species_sampled,
+            e_dna_results_mc,
+            e_dna_results_tubifex
+          ),
+          ~ tidyr::replace_na(.x, "NA")
+        )
+      ) %>%
+      sf::st_drop_geometry()
     
     
-    openxlsx::writeData(my_wb, "Results", results_for_download)
-    openxlsx::setColWidths(my_wb, sheet = 1, cols = 1:ncol(results_for_download), widths = "auto")
+    if(year == "2025") {
+      
+      results <- results %>%
+        dplyr::select(
+          `Waterbody Name` = waterbody_name,
+          `Sample Site` = sample_site_name,
+          Latitude,
+          Longitude,
+          `Sampling Method` = sampling_method,
+          `Fish Species Sampled` = fish_species_sampled,
+          `Fish Sampling Results` = fish_sampling_results_q_pcr_mc_detected,
+          `eDNA Sampling Results (M. cerebralis - parasite)` = e_dna_results_mc,
+          `eDNA Sampling Results (Tubifex worm)` = e_dna_results_tubifex
+        )
+    } else {
+      results <- results %>%
+        dplyr::select(
+          `Sample Site` = sample_site_name,
+          Latitude,
+          Longitude,
+          `Sampling Method` = sampling_method,
+          `Fish Species Sampled` = fish_species_sampled,
+          `Fish Sampling Results` = fish_sampling_results_q_pcr_mc_detected,
+          `eDNA Sampling Results (M. cerebralis - parasite)` = e_dna_results_mc,
+          `eDNA Sampling Results (Tubifex worm)` = e_dna_results_tubifex
+        )
+    }
     
-    species_lookup_tbl = data.frame(
+    results <- results %>% dplyr::arrange(`Sample Site`)
+    
+    openxlsx::writeData(wb, "Results", results)
+    openxlsx::setColWidths(wb, sheet = 1, cols = 1:ncol(results), widths = "auto")
+    
+  } else {
+    openxlsx::writeData(wb, "Results", "No data available for this year")
+  }
+  
+  # Species lookup (same for all years)
+  openxlsx::writeData(
+    wb,
+    "Species Look-up",
+    tibble::tibble(
       acronym = c("BT","EBT","KOK","MW","RBT","SK","WCT"),
-      species = c("Bull Trout","Eastern Brook Trout", "Kokanee",
-                  "Mountain Whitefish","Rainbow Trout","Sockeye Salmon",
-                  "Westslope Cutthroat Trout")
+      species = c(
+        "Bull Trout","Eastern Brook Trout","Kokanee",
+        "Mountain Whitefish","Rainbow Trout",
+        "Sockeye Salmon","Westslope Cutthroat Trout"
+      )
     )
-    
-    openxlsx::writeData(my_wb,"Species Look-up", species_lookup_tbl)
-    
-    
-    openxlsx::saveWorkbook(my_wb, con, overwrite = T)
-  }
+  )
+  
+  openxlsx::saveWorkbook(wb, con, overwrite = TRUE)
+}
+
+output$data_dl_2024 <- downloadHandler(
+  filename = function() { "BC_WhirlingDisease_2024.xlsx" },
+  content = function(con) { create_wb_for_year("2024", con, dat) }
 )
 
-output$WD_analysis_markdown_dl = downloadHandler(
-  filename = function() {
-    paste0('BC_WhirlingDisease_Monitoring_Priority_Assessment_2025.html')
-  },
-  content = function(con) {
-    file.copy("www/wb_prioritization_for_sampling.html", con)
-  }
+output$data_dl_2025 <- downloadHandler(
+  filename = function() { "BC_WhirlingDisease_2025.xlsx" },
+  content = function(con) { create_wb_for_year("2025", con, dat_2025) }
 )
 
-
-output$proposed_locations_dl = downloadHandler(
-  filename = function() {
-    paste0('2025_short_list_map.html')
-  },
-  content = function(con) {
-    file.copy("www/2025_short_list_map.html", con)
-  }
-)
-
-excel_file_name = list.files(path = "www/", pattern = ".xlsx")
-
-
-output$WD_analysis_excel_dl = downloadHandler(
-  filename = function() {
-    paste0(excel_file_name)
-  },
-  content = function(con) {
-    file.copy(paste0("www/",excel_file_name), con)
-  }
-)
