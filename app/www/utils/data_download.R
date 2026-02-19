ae9b0409df5dbf6101bdf83c3adda134a0ad1ab4
Phelan <John.Phelan@gov.bc.ca>
2026-02-19 09:12:25 -0800

    dealing with downloads of volumes and filter sizes

--- app/www/utils/data_download.R
+++ app/www/utils/data_download.R
@@ -21,43 +21,71 @@ create_wb_for_year <- function(year_val, con, dat_dl) {
       )
     
     
-    
-    
     # Column selection differs by year
     if (year == "2025") {
-      
       results <- dat_year %>%
         dplyr::group_by(sample_site_name) %>%
         dplyr::summarise(
           `Sampling Method` = paste(sort(unique(sampling_method)), collapse = " + "),
           waterbody_name = dplyr::first(waterbody_name),
-          fish_species_sampled = tidyr::replace_na(dplyr::first(fish_species_sampled), "NA"),
+          fish_species_sampled = tidyr::replace_na(
+            dplyr::first(fish_species_sampled), "NA"
+          ),
+          `Delivery Agency` = dplyr::first(delivery_agency),
           fish_sampling_results_q_pcr_mc_detected =
-            tidyr::replace_na(dplyr::first(fish_sampling_results_q_pcr_mc_detected), "NA"),
-          e_dna_results_mc = tidyr::replace_na(dplyr::first(e_dna_results_mc), "NA"),
+            tidyr::replace_na(
+              dplyr::first(fish_sampling_results_q_pcr_mc_detected), "NA"
+            ),
+          e_dna_results_mc =
+            tidyr::replace_na(
+              dplyr::first(e_dna_results_mc), "NA"
+            ),
           Latitude  = round(dplyr::first(Latitude), 4),
           Longitude = round(dplyr::first(Longitude), 4),
+          `Date Collected` =
+            paste(sort(unique(date_collected)), collapse = ", "),
+          `Volume Sampled (l)` =
+            paste(sort(unique(volume_sampled)), collapse = ", "),
+          `Filter Size (µm)` =
+            paste(sort(unique(filter_size)), collapse = ", "),
           .groups = "drop"
-        )|> 
+        ) |>
         sf::st_drop_geometry()
+      
+      
+      results <- results %>%
+        tidyr::separate(
+          `Volume Sampled (l)`,
+          into = paste0("Volume Sampled (l) ", 1:3),
+          sep = ", ",
+          fill = "right",
+          extra = "drop"
+        ) %>%
+        tidyr::separate(
+          `Filter Size (µm)`,
+          into = paste0("Filter Size (µm) ", 1:3),
+          sep = ", ",
+          fill = "right",
+          extra = "drop"
+        )
+      
       
       results <- results %>%
         dplyr::select(
-          
           `Waterbody Name` = waterbody_name,
-          
           `Sample Site` = sample_site_name,
           Latitude,
           Longitude,
           `Sampling Method`,
-          
+          `Delivery Agency`,
           `Fish Species Sampled` = fish_species_sampled,
-          
           `Fish Sampling Results` =
             fish_sampling_results_q_pcr_mc_detected,
-          
           `eDNA Sampling Results (M. cerebralis - parasite)` =
-            e_dna_results_mc
+            e_dna_results_mc,
+          `Date Collected`,
+          dplyr::starts_with("Volume Sampled (l)"),
+          dplyr::starts_with("Filter Size (µm)")
         )
       
     } else {
@@ -67,6 +95,7 @@ create_wb_for_year <- function(year_val, con, dat_dl) {
         dplyr::summarise(
           `Sampling Method` = paste(sort(unique(sampling_method)), collapse = " + "),
           fish_species_sampled = tidyr::replace_na(dplyr::first(fish_species_sampled), "NA"),
+          `Delivery Agency` = dplyr::first(delivery_agency),
           fish_sampling_results_q_pcr_mc_detected =
             tidyr::replace_na(dplyr::first(fish_sampling_results_q_pcr_mc_detected), "NA"),
           e_dna_results_mc = tidyr::replace_na(dplyr::first(e_dna_results_mc), "NA"),
@@ -85,7 +114,7 @@ create_wb_for_year <- function(year_val, con, dat_dl) {
           Latitude,
           Longitude,
           `Sampling Method`,
-          
+          `Delivery Agency`,
           `Fish Species Sampled` = fish_species_sampled,
           `Fish Sampling Results` =
             fish_sampling_results_q_pcr_mc_detected,
